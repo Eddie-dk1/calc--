ff4499739fa077b4d39298d9bdc81c6ca5190def
Guka Homelander <70017294+Eddie-dk1@users.noreply.github.com>
2023-09-30 22:19:20 +0300

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	char op;
+	float num1, num2;
+	cout << "Enter the operation(+,-,*,/,^): ";
+	cin >> op;
+
+	cout << "Enter the 1 number: ";
+	cin >> num1;
+
+	cout << "Enter the 2 number: ";
+	cin >> num2;
+
+	switch (op) {
+	case '+':
+		cout << num1 << " + " << num2 << " = " << num1 + num2;
+		break;
+
+	case '-':
+		cout << num1 << " - " << num2 << " = " << num1 - num2;
+		break;
+
+	case '*':
+		cout << num1 << " * " << num2 << " = " << num1 * num2;
+		break;
+
+	case '/':
+		cout << num1 << " / " << num2 << " = " << num1 / num2;
+		break;
+
+	case '^':
+		cout << num1 << " ^ " << num2 << " = " << pow(num1, num2);
+		break;
+	default:
+		cout << "Mistake -_____-";
+		break;
+	}
+
+	return 0;
+}
